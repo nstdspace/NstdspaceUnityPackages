a9a1f32257558cd9d48907da2654fe838dadb25b
Maksim Rotmann <maksim.rotmann@gmail.com>
2023-07-07 21:39:17 +0200

    Add DI stuff

---
+++ NstdspaceUnityCommons/Runtime/Di/AutowiredMonoBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace TilemapTest
+{
+    public class AutowiredMonoBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            var autowireFields = GetType()
+                .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                .Where(f => f.CustomAttributes.Any(a => a.AttributeType == typeof(AutowireAttribute)));
+            foreach (var field in autowireFields)
+            {
+                var injectedObject = ResolveComponent(field);
+                field.SetValue(this, injectedObject);
+            }
+        }
+
+        private Component ResolveComponent(FieldInfo autowiredComponentField)
+        {
+            var componentType = autowiredComponentField.FieldType;
+            var componentOnThis = GetComponent(componentType);
+            if (componentOnThis != null)
+            {
+                return componentOnThis;
+            }
+
+            var componentInChildren = GetComponentInChildren(componentType);
+            if (componentInChildren != null) {
+                return componentInChildren;
+            }
+            
+            return (Component) FindAnyObjectByType(componentType);
+        }
+    }
+}
